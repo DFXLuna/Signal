de07198acf2164149088f30bd517f98444ffa467
DFXLuna <teamuba@gmail.com>
2018-04-11 13:15:39 -0600

    Neuralnetwork + cleanup

--- exp1/main.cpp
+++ exp1/main.cpp
@@ -5,17 +5,25 @@ using std::vector;
 #include<iostream>
 using std::cout;
 using std::endl;
+#include"Ind_NeuralNetwork.hpp"
+#include"Ind_Neuron.hpp"
+
 
 int main(){
-    vector< vector< float > > real = readNumpyArray( "./stft/fund.stft" );
-    vector< vector< float > > written = readNumpyArray( "./outputtest.stft" );
-    for( size_t i = 0; i < real.size(); i++ ){
-        for( size_t j = 0; j < real[i].size(); j++ ){
-            if( real[i][j] != written[i][j] ){
-                cout << "Error at ( " << i << ", " << j << " )" << endl;
-                cout << "Difference of " << real[i][j] - written[i][j] << endl;
-            }
-        }
+    size_t NEURONS = 2050;
+    vector< vector< float > > real = transpose( readNumpyArray( "./stft/fund.stft" ) );
+    NeuralNetwork<> nn( NEURONS );
+    nn.setAllWeightsBiases( 1 );
+    // add connections between inputs (0 - 1024) and outputs(1025 - 2050)
+    // Then set initial input
+    for( size_t i = 0; i < NEURONS / 2; i++ ){
+        nn.addConnection( i, i + NEURONS / 2, 1 );
+        nn.setValue( i, real[0][i] );
+    }
+    nn.step();
+    vector<float> result;
+    for( size_t i = 1025; i < NEURONS; i++ ){
+        result.push_back( nn.getValue( i ) );
     }
 
     return 0;
