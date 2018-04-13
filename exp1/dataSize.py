7cec14546d441395f2f94d77fc985d3efd0d63d4
DFXLuna <teamuba@gmail.com>
2018-04-13 14:12:49 -0600

    Reorganize

--- exp1/dataSize.py
+++
@@ -1,5 +0,0 @@
-import librosa
-
-def printDataSize( data ):
-    print('data size:' + str(data.size))
-    print('data shape:' + str(data.shape))
--- exp1/readSTFT.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-import sys
-
-data = np.loadtxt( sys.argv[1] )
-wav = librosa.core.istft( data )
-
-librosa.output.write_wav( sys.argv[2], wav, 48000 )
